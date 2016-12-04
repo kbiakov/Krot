4cc03f37817fcf76c17e8a6dc889aefe8cb14d06
Kirill Biakov <kiakov@gmail.com>
2016-12-04 23:33:37 +0300

    Updated setup script

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# build API container
+# build containers
+docker build -t kbiakov/krot-notification .
+docker build -t kbiakov/krot-subscription .
 docker build -t kbiakov/krot-api .
+docker build -t kbiakov/proxy .
 
 # create network
 docker network create krot
@@ -15,6 +18,7 @@ docker run -d --net krot -p 4160:4160 -p 4161:4161 nsqio/nsq /nsqlookupd --name 
 # start service containers
 docker run -d --net krot -p 9010:9010 kbiakov/krot-notification --name krot-notification
 docker run -d --net krot -p 9020:9020 kbiakov/krot-subscription --name krot-subscription
-
-# start API container
 docker run -d --net krot -p 5000:5000 kbiakov/krot-api --name krot-api
+
+# start proxy
+docker run -d --net krot -p 50:50 kbiakov/proxy --name proxy
